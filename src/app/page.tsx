a1f2894018d45d8f372740cb1150d1be6e6baec5
k74 <kashhh4u@gmail.com>
2023-07-26 19:46:24 +0100

    Create homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <h1>A better way to practice binary questions.</h1>
+      <Link href="/questions"><button>Start now</button></Link>
     </main>
   )
 }
