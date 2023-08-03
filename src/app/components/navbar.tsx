efc042034285cedb073868e3356bc248145c89fa
k74 <kashhh4u@gmail.com>
2023-08-03 19:42:14 +0100

    Add github logo to navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link"
+import Image from "next/image"
 import styles from "./navbar.module.css"
+import githubIcon from "../../../public/github-mark-white.svg"
 
 export default function Navbar() {
     return(
         <header className={styles.header}>
             <Link href="/"><div className={styles.logo}>BQG</div></Link>
-            <Link href="https://github.com/Bsodoge/Binary-Questions-Generator"><img src="" alt="Github" className="github" /></Link>
+            <Link href="https://github.com/Bsodoge/Binary-Questions-Generator"><Image src={githubIcon.src} alt="Github" width={40} height={40}/></Link>
         </header>
     )
 }
