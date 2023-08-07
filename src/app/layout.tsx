4f18c8416077bf139fa0421ad6d9292d0fea039c
k74 <kashhh4u@gmail.com>
2023-08-07 08:20:12 +0100

    Add google verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,10 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Binary Question Generator',
   description: 'A simple application to help you practice binary problems.',
-  keywords: ['Binary', 'Twos compliment binary', 'BInary fractions', 'Binary practice', 'Binary practice a level', 'binary practice gcse', 'Binary mantissa exponent']
+  keywords: ['Binary', 'Twos compliment binary', 'Binary fractions', 'Binary practice', 'Binary practice a level', 'binary practice gcse', 'Binary mantissa exponent'],
+  verification: {
+    google: "lbLKcs1huBLl10UkYNSxYQwHlVf0z7P5kgPf_wTjj3E",
+  },
 }
 
 export default function RootLayout({
