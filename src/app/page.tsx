66cd20799e16c7a08378e7ef5e25cf4f6609bf09
bsodoge <kashhh4u@gmail.com>
2024-04-17 06:27:26 +0100

    Remove background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>A better way to practice binary questions.</h1>
+      <h1>Binary Question Generator</h1>
+      <h2>A better way to practice binary questions.</h2>
       <Link href="/questions"><button>Start now</button></Link>
     </main>
   )
