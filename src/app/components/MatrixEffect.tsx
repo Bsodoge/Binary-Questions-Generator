f402f1c81753466d772257ba8230e5bae679914e
bsodoge <kashhh4u@gmail.com>
2024-04-18 21:15:00 +0100

    Update homepage

---
+++ src/app/components/MatrixEffect.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+
+export default function MatrixEffect(){
+    const canvas = useRef<HTMLCanvasElement>(null);
+    const setCanvasHeightDimensions = () => {
+        const navbarHeight : number = 80;
+        canvas.current!.height = window.innerHeight - navbarHeight;
+        if(window.innerWidth <= 1580) canvas.current!.width = window.innerWidth;
+        else canvas.current!.width = (window.innerWidth / 2) - 250;
+    }
+    const playEffect = () => {
+        const context : CanvasRenderingContext2D | null | undefined = canvas.current?.getContext("2d");
+        const binary : string = "01";
+        const fontSize : number = 16;
+        const columns : number = Math.floor(canvas.current!.width / fontSize);
+        const yCoords : number[]= new Array(columns).fill(1);
+
+        const draw = () => {
+            context!.fillStyle = "#0000000d";
+            context!.fillRect(0, 0, canvas.current!.width, canvas.current!.height);
+
+            context!.fillStyle = "#0F0";
+            context!.font = `${fontSize}px monospace`;
+
+            for (let i = 0; i < yCoords.length; i++) {
+                const chosenCharacter : string = binary.charAt(Math.floor(Math.random() * binary.length));
+                context!.fillText(chosenCharacter, i * fontSize, fontSize * yCoords[i]);
+                if(yCoords[i] * fontSize > canvas.current!.height && Math.random() > 0.975) yCoords[i] = 0;
+                yCoords[i]++;
+            }
+        }
+        setInterval(draw, 60);
+    }
+    useEffect(() => {
+        setCanvasHeightDimensions();
+        playEffect();
+    }, [])
+    return(
+       <canvas ref={canvas}></canvas> 
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
+import MatrixEffect from './components/MatrixEffect'
 import styles from './page.module.css'
 import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Binary Question Generator</h1>
-      <h2>A better way to practice binary questions.</h2>
-      <Link href="/questions"><button>Start now</button></Link>
+      <div className={styles.text_container}>
+        <h1>Binary Question Generator</h1>
+        <p>The best way to practice binary related questions for GCSE Computer Science, A Level Computer Science and beyond.</p>
+        <Link href="/questions"><button>Start now</button></Link>
+      </div>
+      <MatrixEffect/>
     </main>
   )
 }
