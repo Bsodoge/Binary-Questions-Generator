de5419630abee5ebafa8d0d69e7d3c502d8ac660
bsodoge <kashhh4u@gmail.com>
2024-05-03 16:56:42 +0100

    Update text on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,22 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.text_container}>
         <h1>Binary Question Generator</h1>
-        <p>The best way to practice binary related questions for GCSE Computer Science, A Level Computer Science and beyond.</p>
+        <div className={styles.information_container}>
+          <p>The best way to practice binary related questions for GCSE Computer Science, A Level Computer Science and beyond.</p>
+          <div className={styles.question_types_container}>
+            <p>Practice many types of binary questions including:</p>
+            <ul>
+              <li>Decimal to Binary conversions</li>
+              <li>Hex to Binary conversions</li>
+              <li>Binary Addition</li>
+              <li>Binary Subtraction</li>
+              <li>Binary Multiplication</li>
+              <li>Binary Fractions</li>
+              <li>Two's Compliment Conversions</li>
+              <li>Binary Mantissa and Exponent Questions</li>
+            </ul> 
+          </div>
+        </div>
         <Link href="/questions" title="Start generating binary questions now!"><button>Start now</button></Link>
       </div>
       <MatrixEffect/>
